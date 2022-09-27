16948150d31a6d44120d7a6946399a137316a664
Ahmed Shaker <74692808+Shaker2001@users.noreply.github.com>
2022-09-27 17:17:37 +0200

    Create Two Different Palindromes.cpp

---
+++ C++/codechef/Two Different Palindromes.cpp
@@ -0,0 +1,40 @@
+
+#include<algorithm>
+#include <bits/stdc++.h>
+#define  endl  '\n'
+#define  cinarr0   int n; cin>>n; int arr[n]; for (int i=0; i<n; i++){cin>>arr[i];}
+#define  cinarr1   int n; cin>>n; int arr[n+1]={0}; for (int i=1; i<=n; i++){cin>>arr[i];}
+#define dpp(arr,val) memset(arr,val,sizeof(arr))
+#define  zero   for(int i =0 ; i<n ; i++)
+#define  one   for(int i =1 ; i<=n ; i++)
+#define whilet    int t ; cin>>t ;while(t--)
+#define FIO cin.tie(0), cin.sync_with_stdio(0)
+#define ll   long long
+
+using namespace std;
+
+
+
+int main(){ FIO;
+
+  whilet
+ {
+    ll x,y; cin>>x>>y;
+   if (x==1 ||y ==1)cout<<"No"<<endl;
+   else
+   {
+       if (x %2!=0 && y%2!=0)
+       {
+           cout<<"No"<<endl;
+       }
+       else if (x%2==0 ||y%2==0)
+       {
+           cout<<"Yes"<<endl;
+       }
+       else
+        cout<<"Yes"<<endl;
+
+   }
+ }
+        return 0;
+}
