888cdac1f52c343763decf7d9fb07c74d2d6e49a
Ahmed Shaker <74692808+Shaker2001@users.noreply.github.com>
2022-09-27 17:16:02 +0200

    Create Integers that sum to the same value.cpp

---
+++ C++/codechef/Integers that sum to the same value.cpp
@@ -0,0 +1,25 @@
+
+#include<algorithm>
+#include <bits/stdc++.h>
+#define  endl  '\n'
+#define  cinarr0   int n; cin>>n; int arr[n]; for (int i=0; i<n; i++){cin>>arr[i];}
+#define  cinarr1   int n; cin>>n; int arr[n+1]={0}; for (int i=1; i<=n; i++){cin>>arr[i];}
+#define dpp(arr,val) memset(arr,val,sizeof(arr))
+#define  zero   for(int i =0 ; i<n ; i++)
+#define  one   for(int i =1 ; i<=n ; i++)
+#define whilet    int t ; cin>>t ;while(t--)
+#define FIO cin.tie(0), cin.sync_with_stdio(0)
+#define ll   long long
+
+using namespace std;
+
+
+
+int main(){ FIO;
+
+ // whilet
+ ll x; cin>>x;
+ cout<<x-1;
+
+        return 0;
+}
